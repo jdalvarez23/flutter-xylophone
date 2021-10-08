8ef968dd2d72e5423591d7ba6387fbfa13d9e86d
Jose D. Alvarez <jdalvarez23@gmail.com>
2021-10-08 16:44:10 -0500

    Added audio dependancy, added function to play audio, added colorful buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber) {
+    // create a new audio player
+    final player = AudioCache();
+    // play the file
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int roundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: color
+        ),
+        onPressed: () {
+          playSound(roundNumber);
+        }
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, roundNumber: 1),
+              buildKey(color: Colors.orange, roundNumber: 2),
+              buildKey(color: Colors.yellow, roundNumber: 3),
+              buildKey(color: Colors.green, roundNumber: 4),
+              buildKey(color: Colors.teal, roundNumber: 5),
+              buildKey(color: Colors.blue, roundNumber: 6),
+              buildKey(color: Colors.purple, roundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
